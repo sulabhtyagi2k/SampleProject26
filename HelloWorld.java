e229b3c4280a290f85563ed629eea912831e8396
SulabhTyagi <43522557+sulabhtyagi2k@users.noreply.github.com>
2024-10-04 14:45:21 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,5 @@
-class Hello World {
-public static void main (String[] args) {
-  
-    System.out.println("Hello, World!");
-   
-  }
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
 }
